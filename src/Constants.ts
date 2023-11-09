82653823d822a09d2a0f634ff820795fff35fcc1
Aiden Kennedy <awkennedy03@aol.com>
2023-11-09 18:36:41 -0500

    testing

--- src/Constants.ts
+++ src/Constants.ts
@@ -5,7 +5,7 @@ const { height, width } = Dimensions.get("screen");
 const CONSTANTS = {
   ScreenHieght: height,
   ScreenWidth: width,
-  URL: "https://c393-2620-8d-8000-1064-c836-9f57-74e6-f04d.ngrok-free.app/", // Must include '/' at the end of url
+  URL: "https://fdc0-69-193-26-42.ngrok-free.app/", // Must include '/' at the end of url
   GeoCodeURL: "https://maps.googleapis.com/maps/api/geocode/json?address=",
   GoogleAPIKey: "AIzaSyDZXuezXmHuoBbecqGqZz0IW0kv7S65Pes", // This will be deleted at the end of the semester https://console.cloud.google.com/google/maps-apis/home?project=swen-444
 };

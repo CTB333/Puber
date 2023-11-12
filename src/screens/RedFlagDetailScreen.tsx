889d09d6ff17a969ffcac3d3a077d0a65345e91f
Aiden Kennedy <awkennedy03@aol.com>
2023-11-12 01:07:52 -0500

    Saving changes to change branch

--- src/screens/RedFlagDetailScreen.tsx
+++ src/screens/RedFlagDetailScreen.tsx
@@ -1,13 +1,30 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
 import STYLES from "../styles";
 import { RedFlagDetailScreenProps } from "../navigation";
 import { useGoBackHeader } from "../hooks";
+import redFlagImage from "../assets/redflag.png";
 
-const RedFlagDetailScreen = ({ navigation }: RedFlagDetailScreenProps) => {
-  useGoBackHeader();
+const RedFlagDetailScreen = ({ navigation, route }: RedFlagDetailScreenProps) => {
+  useGoBackHeader();;
+
   return (
-    <View style={[STYLES.page, STYLES.center]}>
-      <Text>RedFlagDetailScreen</Text>
+    <View style={[
+      STYLES.row,
+      STYLES.width,
+      STYLES.center,
+      { justifyContent: "space-between" },
+    ]}>
+      <Image style={{ width: 150, height: 150 }} source={redFlagImage} />
+      <Text
+        style={[
+          STYLES.colorPrimary,
+          STYLES.fs1,
+          STYLES.bold,
+          { textAlign: "center", maxWidth: "45%" },
+        ]}
+      >
+        {}
+      </Text>
     </View>
   );
 };
--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,13 +1,31 @@
 import { View, Text, StyleSheet } from "react-native";
 import STYLES from "../styles";
 import { UserScreenProps } from "../navigation";
-import { useAddFlagHeader, useGoBackHeader } from "../hooks";
+import { useAddFlagHeader, useGetAllRedFlags, useGoBackHeader } from "../hooks";
+import { RedFlagSnippet } from "../components";
+import { RedFlag, RedFlagData } from "../interfaces";
 
 const UserScreen = ({ navigation, route }: UserScreenProps) => {
+
+  /**I added code here so I could access the red flags, all of it is non ideal please get rid of it
+   * it doesn't even filter to just the red flags for a person, everyone has every red flag
+   */
+
+  const redFlags = useGetAllRedFlags().flags
+
+  const toFlag = (flag: RedFlag) =>{
+    navigation.navigate("RedFlagDetail", flag);
+  }
+
   useAddFlagHeader(route.params.user);
   return (
     <View style={[STYLES.page, STYLES.center]}>
       <Text>UserScreen</Text>
+      {redFlags.map((redFlag) => {
+        return (
+          <RedFlagSnippet onPress={() => toFlag(redFlag)} key={redFlag.id} flag={redFlag} />
+        );
+      })}
     </View>
   );
 };

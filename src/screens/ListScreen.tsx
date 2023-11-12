aea721f124c8cb6232f2d283e607a0c552c459ae
Colin Tondreau <cdt8588@rit.edu>
2023-11-12 06:25:26 -0500

    Looks good, all I did was link the party to party nav

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -9,7 +9,12 @@ import {
   usePartyNav,
 } from "../hooks";
 import { useSetHeader } from "../hooks";
-import { FormHeader, PartySnippet, PartySnippetList, UserSnippet } from "../components";
+import {
+  FormHeader,
+  PartySnippet,
+  PartySnippetList,
+  UserSnippet,
+} from "../components";
 import { useUser } from "../providers";
 import { ListScreenProps } from "../navigation";
 import { Party, User } from "../interfaces";
@@ -19,22 +24,15 @@ const ListScreen = ({ navigation }: ListScreenProps) => {
   useEnableDrawerSwipe();
   useDrawerHeader();
 
-  // const { user } = useUser();
   const { users } = useGetAllUsers();
-  //const { parties } = useGetAllParties();
-  const { parties, distances } = useFilterParties([
-    navigation.isFocused(),
-  ]);
+
+  const { parties, distances } = useFilterParties([navigation.isFocused()]);
 
   const toUser = (user: User) => {
     navigation.navigate("User", { user });
   };
 
   const toParty = usePartyNav();
-
-  // const toParty = (party: Party) => {
-  //   navigation.navigate("Party", {party});
-  // }
 
   return (
     <View style={[STYLES.page, STYLES.center]}>
@@ -43,23 +41,28 @@ const ListScreen = ({ navigation }: ListScreenProps) => {
         <ScrollView horizontal style={[STYLES.width]}>
           {users.map((user) => {
             return (
-              <View style={[{marginRight: 15}]}>
-                <UserSnippet onPress={() => toUser(user)} key={user.id} user={user} />
+              <View key={user.id} style={[{ marginRight: 15 }]}>
+                <UserSnippet onPress={() => toUser(user)} user={user} />
               </View>
             );
           })}
         </ScrollView>
         <FormHeader title="Parties" />
-        {/* <PartySnippetList parties={parties}/> */}
         {parties.map((party) => {
-            return (
-              <View style={[{ marginBottom: 15 }]} key={party.id}>
-                <PartySnippet primaryColor party={party} distance={distances.find((distance) => party.id === distance.partyId)} />
-              </View>
-            );
-          })}
+          return (
+            <View style={[{ marginBottom: 15 }]} key={party.id}>
+              <PartySnippet
+                onPress={() => toParty(party)}
+                primaryColor
+                party={party}
+                distance={distances.find(
+                  (distance) => party.id === distance.partyId
+                )}
+              />
+            </View>
+          );
+        })}
       </ScrollView>
-      
     </View>
   );
 };
